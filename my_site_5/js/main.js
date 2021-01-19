70b6ab4afa89e1e0f9a5dd25d0027bcc62f6928b
frame-by <75788708+frame-by@users.noreply.github.com>
2021-01-19 20:27:39 +0900

    Update main.js

--- my_site_5/js/main.js
+++ my_site_5/js/main.js
@@ -181,33 +181,33 @@
 	// 	// sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
 	// }
 
-	// function calcValues(values, currentYOffset) {
-	// 	let rv;
-	// 	// 현재 씬(스크롤섹션)에서 스크롤된 범위를 비율로 구하기
-	// 	const scrollHeight = sceneInfo[currentScene].scrollHeight;
-	// 	const scrollRatio = currentYOffset / scrollHeight;
+	function calcValues(values, currentYOffset) {
+		let rv;
+		// 현재 씬(스크롤섹션)에서 스크롤된 범위를 비율로 구하기
+		const scrollHeight = sceneInfo[currentScene].scrollHeight;
+		const scrollRatio = currentYOffset / scrollHeight;
 
-	// 	if (values.length === 3) {
-	// 		// start ~ end 사이에 애니메이션 실행
+		if (values.length === 3) {
+			// start ~ end 사이에 애니메이션 실행
 
-	// 		// 이거 지우면 실행 안됨
-	// 		const partScrollStart = values[2].start * scrollHeight;
-	// 		const partScrollEnd = values[2].end * scrollHeight;
-	// 		const partScrollHeight = partScrollEnd - partScrollStart;
+			// 이거 지우면 실행 안됨
+			const partScrollStart = values[2].start * scrollHeight;
+			const partScrollEnd = values[2].end * scrollHeight;
+			const partScrollHeight = partScrollEnd - partScrollStart;
 
-	// 		if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
-	// 			rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
-	// 		} else if (currentYOffset < partScrollStart) {
-	// 			rv = values[0];
-	// 		} else if (currentYOffset > partScrollEnd) {
-	// 			rv = values[1];
-	// 		}
-	// 	} else {
-	// 		rv = scrollRatio * (values[1] - values[0]) + values[0];
-	// 	}
+			if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
+				rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
+			} else if (currentYOffset < partScrollStart) {
+				rv = values[0];
+			} else if (currentYOffset > partScrollEnd) {
+				rv = values[1];
+			}
+		} else {
+			rv = scrollRatio * (values[1] - values[0]) + values[0];
+		}
 
-	// 	return rv;
-	// }
+		return rv;
+	}
 
 	function playAnimation() {
 		const objs = sceneInfo[currentScene].objs;
