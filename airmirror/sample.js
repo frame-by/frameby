e834c3e8a46c9056c53a55fbd94c0d0186092fe5
frame-by <75788708+frame-by@users.noreply.github.com>
2021-02-16 10:43:29 +0900

    Add files via upload

---
+++ airmirror/sample.js
@@ -0,0 +1,45 @@
+// JavaScript Document
+
+
+const html = document.documentElement;
+const canvas = document.getElementById("hero-lightpass-0");
+const context = canvas.getContext("2d");
+
+const frameCount = 201;
+const currentFrame = index => (
+  `img_airbmirror/${index.toString().padStart(4, '0')}.jpg`
+)
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+const img = new Image()
+img.src = currentFrame(1);
+canvas.width=2560;
+canvas.height=1440;
+img.onload=function(){
+  context.drawImage(img, 0, 0);
+}
+
+const updateImage = index => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+}
+
+window.addEventListener('scroll', () => {  
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+  
+  requestAnimationFrame(() => updateImage(frameIndex + 1))
+});
+
+preloadImages()
